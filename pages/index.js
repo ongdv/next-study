cce1d4d024a97e61b16242c1bbc233af3816f69a
ongdv <ongdv@pusan.ac.kr>
2023-07-17 15:19:20 +0900

    [UPDATE] Update ServerSideRendering
    - 서버사이드 렌더링 적용

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,26 @@
 // 이런식으로 import는 컴포넌트에서 불가능하며, module에서만 가능
 // _app.js에서 import 해야함
 // import '../styles/globals.css'
-import Seo from '@/components/Seo';
-import { useEffect, useState } from 'react';
+import Seo from "@/components/Seo";
+import { useEffect, useState } from "react";
 const AUTH_KEY = process.env.AUTH_KEY;
-export default function Home() {
-  /* Router */
-  /* State */
-  const [movies, setMovies] = useState();
-  /* Functions */
-  /* Hooks */
-  useEffect(() => {
-    (async () => {
-      // const url = 'https://api.themoviedb.org/3/movie/popular';
-      const url = '/api/movies';
-      
-      // const options = {
-      //   method: 'GET',
-      //   headers: {
-      //     accept: 'application/json',
-      //     Authorization: `Bearer ${AUTH_KEY}`
-      //   }
-      // };
-
-      // const {results} = await (await fetch(url, options)).json();
-      const {results} = await (await fetch(url)).json();
-      setMovies(results);
-    })()
-  
-  }, [])
-  
-  /* Render */
+export default function Home({results}) {
+/* Router */
+/* State */
+/* Functions */
+/* Hooks */
+/* Render */
   return (
-    <div className='container'>
+    <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => {
+      {!results && <h4>Loading...</h4>}
+      {results?.map((movie) => {
         return (
           <div className="movie" key={movie.id}>
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.id} />
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.id}
+            />
             <h4>{movie.original_title}</h4>
           </div>
         );
@@ -49,6 +31,9 @@ export default function Home() {
           grid-template-columns: 1fr 1fr;
           padding: 20px;
           gap: 20px;
+        }
+        .movie{
+          cursor: pointer;
         }
         .movie img {
           max-width: 100%;
@@ -66,4 +51,20 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+// ServerSideRendering을 지원하는 함수
+// 이 부분은, 서버에서 제공되며, 위의 React의 경우 HTML로 빌드되어 넘어감
+// 해당 함수의 이름은 바뀌면 안됨!
+// rewrite, redirect를 사용했을 경우, 호스트네임을 적어줘야함
+export async function getServerSideProps() {
+
+  const url = 'http://localhost:3000/api/movies'
+  const { results } = await (await fetch(url)).json();
+
+  return {
+    props: {
+      results,
+    },
+  };
 }
