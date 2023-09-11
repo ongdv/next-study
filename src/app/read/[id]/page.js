a2bb5bdd91bbe038893f07e77d6a8b52595f4d90
ongdv <ongdv@pusan.ac.kr>
2023-09-11 17:18:29 +0900

    [UPDATE] 수정 기능 구현
    - 수정 기능 구현
    - 수정 후 읽기 기능 개선
      캐시 처리

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,5 +1,7 @@
 export default async function Read({ params: { id } }) {
-  const resp = await fetch(`http://localhost:9999/topics/${id}`);
+  const resp = await fetch(`http://localhost:9999/topics/${id}`, {
+    cache: 'no-cache',
+  });
   const topic = await resp.json();
   return (
     <>
