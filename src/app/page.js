051ec6cf1e1538e05d2846c8a7531d1e0bd27320
ongdv <ongdv@pusan.ac.kr>
2023-09-11 16:45:41 +0900

    [UPDATE] Create Skeleton
    뼈대 만들기

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <>Hello, Nextjs!</>
-  )
+    <>
+      <h2>Welcome</h2>
+      Hello, WEB
+    </>
+  );
 }
