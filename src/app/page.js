d3c605b9bfacaa1f751df8c1ee48b50f9efe3f5b
ongdv <ongdv@pusan.ac.kr>
2023-09-11 16:54:43 +0900

    [UPDATE] Using Static Resource

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
-import Image from 'next/image';
-
 export default function Home() {
   return (
     <>
       <h2>Welcome</h2>
       Hello, WEB
+      <p>
+        <img src="/hello.png" width="80" alt="" />
+      </p>
     </>
   );
 }
